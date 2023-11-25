7d72651dedf1e31c031f29208c012a1361a89034
Maxime MUNARI <88986446+MaxM743@users.noreply.github.com>
2023-11-25 07:24:23 +0100

    Delete header.php

--- header.php
+++
@@ -1,4 +0,0 @@
-<header>
-  <button class="header-button" onclick='window.location.href="index.php"'>Home</button>
-  <button class="header-button" onclick='window.location.href="projects.php"'>Projects</button>
-</header>
